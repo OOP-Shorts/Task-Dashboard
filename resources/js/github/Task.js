09899fb8aae411104c1030b66a0d5fe49d576414
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-20 15:02:13 +0100

    Add progress tracking

--- resources/js/github/Task.js
+++ resources/js/github/Task.js
@@ -1,5 +1,15 @@
+/* global showdown */
+
 const STARTER_DOWNLOAD = "https://github.com/$FULL_NAME/archive/refs/heads/starter.zip",
     SOLUTION_DOWNLOAD = "https://github.com/$FULL_NAME/archive/refs/heads/solution.zip";
+
+let converter = new showdown.Converter();
+
+async function getHintsFromRepo(repo) {
+    let reponse = await fetch(`https://raw.githubusercontent.com/${repo.full_name}/starter/README.md`),
+        text = await reponse.text();
+    return converter.makeHtml(text);
+}
 
 class Task {
 
@@ -12,19 +22,41 @@ class Task {
         this.starter = starter;
         this.solution = solution;
         this.support = support;
-        Object.freeze(this);
+    }
+
+    getHints() {
+        return new Promise((resolve, reject) => {
+            if(this.hints) {
+                resolve(this.hints);
+            } else {
+                let check = setInterval(() => {
+                    if(this.hints) {
+                        resolve(this.hints);
+                        clearInterval(check);
+                        clearTimeout(timeout);
+                    }
+                }, 500),
+                timeout = setTimeout(() => { 
+                    clearInterval(check);
+                    reject();
+                }, 5000);
+            }
+        });
+        
     }
 
     static fromRepo(repo) {
         let id = repo.id,
             title = repo.name.substring(repo.name.indexOf("-") + 1).replaceAll("-", " "),
-            category = repo.homepage.substring(repo.homepage.lastIndexOf("#") + 1).replaceAll("-", " "),
+            category = repo.topics.find((topic) => topic.startsWith("category-")).replace("category-", "").replaceAll("-", " "),
             description = repo.description,
-            topics = repo.topics,
+            topics = repo.topics.filter((topic) => topic !== "published" && !topic.startsWith("category-")),
             starter = STARTER_DOWNLOAD.replace("$FULL_NAME", repo.full_name),
             solution = SOLUTION_DOWNLOAD.replace("$FULL_NAME", repo.full_name),
-            support = "";
-        return new Task(id, title, category, description, topics, starter, solution, support);
+            support = repo.homepage,
+            task = new Task(id, title, category, description, topics, starter, solution, support);
+        getHintsFromRepo(repo).then((hints) => task.hints = hints);
+        return task;
     }
 
 }
--- resources/js/ui/TaskView.js
+++ resources/js/ui/TaskView.js
@@ -1,7 +1,23 @@
 const TASK_TEMPLATE = document.querySelector("#task-template").innerHTML.trim();
 
-function onTaskSelected(event) {
+let completedTasks = JSON.parse(localStorage.getItem("COMPLETED_TASK")) || [];
+
+function addCompletedTaskToStorage(taskId) {
+    if(completedTasks.includes(taskId)) {
+        return;
+    }
+    completedTasks.push(taskId);
+    localStorage.setItem("COMPLETED_TASK", JSON.stringify(completedTasks));
 }
+
+function removeCompletedTaskFromStorage(taskId) {
+    if(!completedTasks.includes(taskId)) {
+        return;
+    }
+    completedTasks.splice(completedTasks.indexOf(taskId), 1);
+    localStorage.setItem("COMPLETED_TASK", JSON.stringify(completedTasks));
+}
+
 
 class TaskView {
 
@@ -9,8 +25,17 @@ class TaskView {
         this.root = el;
     }
 
-    onTaskSelected(event) {
-        console.log(`Task selected: ${event.target.getAttribute("data-id")}`);
+    toggleHints(taskEl) {
+        taskEl.querySelector(".hints").classList.toggle("closed");
+    }
+
+    toggleStatus(taskEl) {
+        taskEl.classList.toggle("closed");
+        if(taskEl.classList.contains("closed")) {
+            addCompletedTaskToStorage(taskEl.getAttribute("data-id"));
+        } else {
+            removeCompletedTaskFromStorage(taskEl.getAttribute("data-id"));
+        }
     }
 
     append(task) {
@@ -18,18 +43,25 @@ class TaskView {
         taskEl.innerHTML = TASK_TEMPLATE;
         taskEl = taskEl.firstChild;
         taskEl.setAttribute("data-id", task.id);
-        taskEl.querySelector(".category").innerHTML = task.category;
+        taskEl.querySelector(".category .text").innerHTML = task.category;
         taskEl.querySelector(".title").innerHTML = task.title;
         taskEl.querySelector(".description").innerHTML = task.description;
-        taskEl.querySelector("[data-action=\"starter\"] a").href = task.starter; 
-        taskEl.querySelector("[data-action=\"solution\"] a").href = task.solution; 
-        taskEl.querySelector("[data-action=\"support\"] a").href = task.support; 
-        taskEl.querySelector("[data-action=\"more\"]").addEventListener("click", () => this.onTaskSelected());
+        taskEl.querySelector("[data-action=\"starter\"] a").href = task.starter;
+        taskEl.querySelector("[data-action=\"solution\"] a").href = task.solution;
+        taskEl.querySelector("[data-action=\"support\"] a").href = task.support;
+        task.getHints().then((hints) => {
+            taskEl.querySelector(".hints .text").innerHTML = hints;
+        });
         task.topics.forEach(topic => {
             let topicEl = document.createElement("li");
             topicEl.innerHTML = topic;
             taskEl.querySelector(".topics").append(topicEl);
         });
+        taskEl.querySelector(".menu").addEventListener("click", () => this.toggleHints(taskEl));
+        taskEl.querySelector(".category i").addEventListener("click", () => this.toggleStatus(taskEl));
+        if(completedTasks.includes(task.id.toString())) {
+            this.toggleStatus(taskEl);
+        }
         this.root.append(taskEl);
     }
 
