a732032ba2c8354d1beef505ac876fe311ebef33
Alexander Bazo <alexanderbazo@googlemail.com>
2021-12-21 16:41:21 +0100

    Add onboarding (with introjs)

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,10 +1,14 @@
 import TaskService from "./github/TaskService.js";
 import TaskView from "./ui/TaskView.js";
+import TourGuide from "./ui/TourGuide.js";
 
 function renderTasks(tasks) {
   tasks.forEach(task => {
     TaskView.append(task);
   });
+  let tour = new TourGuide();
+  tour.prepare();
+  tour.start();
 }
 
 TaskView.setRoot(document.querySelector(".tasks"));
